8de067f136d936bb1109a80cc332c02ba7f66a32
root <marcos.sob@hotmail.com>
2020-11-01 16:59:46 +0000

    working version

--- clone.py
+++ clone.py
@@ -7,34 +7,45 @@ with open('/opt/carnd_p3/data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
-        
-print("size of lines is: ", len(lines))
-print("steering is: ", lines[1][3])
 
 lines.pop(0)
         
 images = []
 measurements = []
 for line in lines:
-    print('/opt/carnd_p3/data/' + line[0])
     image = cv2.imread('/opt/carnd_p3/data/' + line[0])
     measurement = float(line[3])
     
     images.append(image)
     measurements.append(measurement)
+    images.append(np.fliplr(image))
+    measurements.append(-1.0*measurement)
     
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Dropout, Conv2D, Lambda, Cropping2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x : (x / 255) - 0.5))
+
+model.add(Conv2D(24, kernel_size=5, strides=2, activation='relu'))
+model.add(Conv2D(36, kernel_size=5, strides=2, activation='relu'))
+model.add(Conv2D(48, kernel_size=5, strides=2, activation='relu'))
+model.add(Conv2D(64, kernel_size=3, strides=1, activation='relu'))
+model.add(Conv2D(64, kernel_size=3, strides=1, activation='relu'))
+
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
+print("success")
     
